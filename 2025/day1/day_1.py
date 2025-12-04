6acc8f3d06352db72feec3a06fb2434cbbd4f14e
DRowbo990 <davidrowbotham98@gmail.com>
2025-12-03 21:57:12 -0700

    rm extra code

--- 2025/day1/day_1.py
+++ 2025/day1/day_1.py
@@ -9,7 +9,7 @@ with open("input.txt", "r", encoding="utf-8") as f:
         if "L" in line:
             num -= int(re.findall(r"\d+", line)[0])
         elif "R" in line:
-            num += int(re.findall(r"\d+", line)[0]) % 100
+            num += int(re.findall(r"\d+", line)[0])
         num %= 100
         if num == 0:
             count += 1
