73a9e2fb46abb3137c6f2961a2afb8b4d69bdccc
drowbotham <davidrowbotham98@gmail.com>
2025-12-04 22:46:57 -0700

    Part 2 was challenging

--- 2025/day1/day_1.py
+++ 2025/day1/day_1.py
@@ -1,17 +1,22 @@
 import re
 
-numbers = list(range(100))
 num = 50
 count = 0
 with open("input.txt", "r", encoding="utf-8") as f:
     content = f.read().splitlines()
     for line in content:
+        number = int(re.findall(r"\d+", line)[0])
         if "L" in line:
-            num -= int(re.findall(r"\d+", line)[0])
+            for i in range(number):
+                num -= 1
+                if num == 0:
+                    count += 1
+                if num < 0:
+                    num = 99
         elif "R" in line:
-            num += int(re.findall(r"\d+", line)[0])
-        num %= 100
-        if num == 0:
-            count += 1
-
+            for i in range(number):
+                num += 1
+                if num > 99:
+                    num = 0
+                    count += 1
 print(count)
